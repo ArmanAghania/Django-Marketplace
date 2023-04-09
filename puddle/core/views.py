1782b070bf85b6ff45de0508b09f65940be97e2b
ArmanAghania <Arman94.vergil94@gmail.com>
2023-04-09 23:41:29 +0300

    Login/Logout

--- puddle/core/views.py
+++ puddle/core/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from item.models import Category, Item
+from .forms import SignUpForm
+
 # Create your views here.
 def index(request):
     items = Item.objects.filter(is_sold=False)[0:6]
@@ -11,3 +13,18 @@ def index(request):
 
 def contact(request):
     return render(request, 'core/contact.html')
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+
+            return redirect('/login/')
+    else:
+        form = SignUpForm()
+    
+    return render(request, 'core/signup.html',{
+        'form': form
+    })
--- puddle/puddle/urls.py
+++ puddle/puddle/urls.py
@@ -16,12 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from core.views import index, contact
 from django.conf import settings
 from django.conf.urls.static import static
+
 urlpatterns = [
-    path('', index, name='index'),
-    path('contact/', contact, name='contact'),
+
+    path('', include('core.urls')),
     path('item/', include('item.urls')),
     path("admin/", admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
